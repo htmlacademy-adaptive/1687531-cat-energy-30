fb1f4b1187856e2b7677bcd8f754af5836996616
Aleksandr <saneksa01@gmail.com>
2024-01-17 23:10:47 +0300

    Добавил js для кнопки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,19 @@
-/* в этот файл добавляет скрипты*/
+const menu = document.querySelector(".nav");
+const menuButton = document.querySelector(".nav__toggle");
+const navMenu = document.querySelector(".nav__list");
+
+
+const toggleMenu = () => {
+    if (menu.classList.contains("nav--closed")) {
+        menu.classList.remove("nav--closed");
+        menu.classList.add("nav--opened");
+        navMenu.style.display = "block";
+    } else {
+        menu.classList.remove("nav--opened");
+        menu.classList.add("nav--closed");
+        navMenu.style.display = "none";
+    }
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
